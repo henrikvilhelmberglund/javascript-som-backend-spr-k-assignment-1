48bda86d7a4c5955f55a4da809321dc46314ce1d
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-05-04 11:31:06 +0200

    fix: :package: remove tailwindcss and use unocss instead

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./views/**/*.{html,js,ejs}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")],
-};
